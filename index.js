45357872e6d990892b5f737c323c727afa3a94b7
Thomas Maximini <tmaximini@gmail.com>
2020-06-13 11:53:22 +0200

    upload files to S3

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { exec: _exec } = require("child_process");
 const { promisify } = require("util");
-
 const { schedule } = require("node-cron");
+const path = require('path')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.S3_KEY_ID,
+  secretAccessKey: process.env.S3_KEY_SEC
+});
+
 
 const exec = promisify(_exec);
+const token = process.env.FIREBASE_TOKEN;
 
 const run = async (cmd) => {
   console.log(`--> ${cmd}`);
@@ -17,8 +26,28 @@ const main = async () => {
   console.log(time);
 
   await run(
-    `firebase database:profile --project firebase-crowdcast -o logs/${time}.txt -d 120`
+    `firebase database:profile --token ${token} --project firebase-crowdcast -o logs/${time}.txt -d 120`
   );
+
+  const filePath = path.join(__dirname, `./logs/${time}.txt`)
+
+  const fileContent = fs.readFileSync(filePath);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: 'cc-firebase-profiler',
+        Key: `${time}.txt`, // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+          console.log("Error uploading to S3", error)
+        } else {
+        console.log(`File uploaded successfully. ${data.Location}`);
+      }
+    });
 };
 
 schedule("*/2 * * * *", main);
